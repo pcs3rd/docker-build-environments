05afa10527cd13db7bbe84b479d5b0ce3cb5f998
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2023-11-12 16:46:51 -0500

    cancel halium sync. Leave that up to a bind mount

--- scripts/halium/halium_home_init.sh
+++ scripts/halium/halium_home_init.sh
@@ -9,7 +9,4 @@ chmod a+rx ~/bin/repo
 export PATH="$HOME/bin:$PATH" >> ~/.bashrc
 source $HOME/.bashrc
 mkdir $HOME/halium && cd $HOME/halium
-echo "Halium repo init"
-repo init -u https://github.com/Halium/android -b halium-13.0 --depth=1
-echo "Halium repo sync"
-repo sync -c -j 16
+
