746b6936aa3abd6e221fc6eea38e184f99b58cc3
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2023-11-12 11:31:03 -0500

    ubuntu-halium [scripts]:libgl1-mesa-glx:i386 isn't available in ubuntu repos anymore.

--- scripts/halium/halium_install_deps.sh
+++ scripts/halium/halium_install_deps.sh
@@ -3,7 +3,7 @@ dpkg --add-architecture i386
 apt-get update 
 apt-get -y install git gnupg flex bison gperf build-essential \
   zip bzr curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
-  libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
+  libx11-dev:i386 libreadline6-dev:i386 \
   libgl1-mesa-dev g++-multilib mingw-w64-i686-dev tofrodos \
   python3-markdown libxml2-utils xsltproc zlib1g-dev:i386 schedtool \
   liblz4-tool bc lzop imagemagick rsync \
